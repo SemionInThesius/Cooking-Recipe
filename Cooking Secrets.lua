ace813812897c8e6db537c13e91de06f771c5606
SemionInThesius <dennisraymonrogerson@gmail.com>
2025-05-28 20:55:31 +0300

    Update Cooking Secrets.lua

--- Cooking Secrets.lua
+++ Cooking Secrets.lua
@@ -45,8 +45,8 @@ local scriptMap = {
     },
 }
 
-local universalKey = "iloveniggers"
-local expiredKey = "plsfreekeydaddy"
+local universalKey = "itsrealyhard"
+local expiredKey = "iloveniggers"
 
 
 local a1 = {104,116,116,112,115,58,47,47,100,105,115,99,111,114,100,46,99,111,109,47}
